dde8be15e62e608cd0fb1c1565c7463ff544713d
qq596670409 <59667409@qq.com>
2016-09-02 17:57:07 +0800

    Revert "Second"
    This reverts commit 1f422679404939f89b994a704fcfe92a7ea8a442.

--- app/src/main/java/com/baoyu/betterweather2016/util/HttpCallbackListener.java
+++
@@ -1,9 +0,0 @@
-package com.baoyu.betterweather2016.util;
-
-/**
- * Created by Administrator on 2016/9/2.
- */
-public interface HttpCallbackListener {
-    void onFinish(String response);
-    void onError(Exception e);
-}
